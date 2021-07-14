a30c9a47a4fdf20ffb2c866c0af01fb3cd9712fd
IrennaLumbuun <irennanicole@yahoo.com>
2021-07-14 09:51:19 -0700

    Generated changes from SM

--- examples/filters.go
+++ examples/filters.go
@@ -12,7 +12,7 @@ import (
 func main() {
 
 	// login to Array, get groupService instance
-	groupService, err := service.NewNsGroupService("1.1.1.1", "xxx", "xxx", "v1", false)
+	groupService, err := service.NewNsGroupService("10.157.82.90", "raunak", "Nim123Boli", "v1", false, true)
 	if err != nil {
 		fmt.Printf("NewGroupService(): Unable to connect to group, err: %v", err.Error())
 	}
